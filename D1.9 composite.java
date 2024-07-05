7581125b4b36194d433d45f3ee2e59011f6e1769
Eragamreddy720 <122278130+Eragamreddy720@users.noreply.github.com>
2024-07-05 14:01:02 +0530

    D1.9 composite.java

---
+++ D1.9 composite.java
@@ -0,0 +1,24 @@
+import java.util.Scanner;
+
+public class composite1729{
+    public static void main(String args[]){
+    
+    Scanner input=new Scanner(System.in);
+    int a=input.nextInt();
+    int b=input.nextInt();
+    
+    for(int i=a+1;i<=b;i++){
+        int c=0;
+        for(int j=1;j<=b;j++)
+        {
+            if(i%j==0)
+            c++;
+            
+        }
+        if(c>2)
+        System.out.print(i+" ");
+        
+    }
+
+}
+}
